faf0f067216f1d17797fa84b68e4ed60d02d5a6f
Dan Meliza <dan@meliza.org>
2016-11-09 21:02:23 -0500

    add bulk import script

---
+++ scripts/bulk_import.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# -*- mode: python -*-
+
+import json
+import requests as rq
+
+if __name__=="__main__":
+    import argparse
+
+    p = argparse.ArgumentParser()
+    p.add_argument("endpoint", help="URL to POST records to")
+    p.add_argument("records", help="file with records as JSON array")
+
+    args = p.parse_args()
+
+    data = json.load(open(args.records, 'r'))
+    for record in data:
+        print(record)
+        r = rq.post(args.endpoint, json=record)
+        r.raise_for_status()
