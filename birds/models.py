479c1bb6f494c825f863f69c4ad449f4338ecbf4
Dan Meliza <dan@meliza.org>
2017-06-22 17:02:33 -0400

    show last location - somewhat inefficient

--- birds/models.py
+++ birds/models.py
@@ -92,6 +92,13 @@ class LivingAnimalManager(models.Manager):
         return super(LivingAnimalManager, self).get_queryset().exclude(event__status__count=-1)
 
 
+class LastEventManager(models.Manager):
+    """ Filters queryset so that only the most recent event is returned """
+    def get_queryset(self):
+        qs = super(LastEventManager, self).get_queryset()
+        return qs.order_by("animal_id", "-date").distinct("animal_id")
+
+
 @python_2_unicode_compatible
 class Parent(models.Model):
     child = models.ForeignKey('Animal', related_name="+", on_delete=models.CASCADE)
@@ -185,6 +192,10 @@ class Animal(models.Model):
         except AttributeError:
             pass
 
+    def last_location(self):
+        """ Returns the location recorded in the most recent event """
+        return self.event_set.order_by("-date").first().location
+
     def get_absolute_url(self):
         return reverse("birds:animal", kwargs={'uuid': self.uuid})
 
@@ -203,6 +214,9 @@ class Event(models.Model):
     entered_by = models.ForeignKey(settings.AUTH_USER_MODEL,
                                    on_delete=models.SET(get_sentinel_user))
     created = models.DateTimeField(auto_now_add=True)
+
+    objects = models.Manager()
+    latest = LastEventManager()
 
     def __str__(self):
         return "%s: %s on %s" % (self.animal, self.status, self.date)
