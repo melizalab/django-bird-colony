762b278f1fe6f2a78ccebe672cdcf371e8421ebc
Dan Meliza <dan@meliza.org>
2019-06-11 20:30:21 -0400

    tick version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.hexversion < 0x03050000:
     raise RuntimeError("Python 3.5 or higher required")
 
 
-VERSION = '0.7.3'
+VERSION = '0.7.4'
 cls_txt = """
 Development Status :: 4 - Beta
 Framework :: Django
