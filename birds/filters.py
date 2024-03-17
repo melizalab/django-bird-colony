496df798303d2dda0b27e559e87a76b6ca15e0f2
Dan Meliza <dan@meliza.org>
2024-03-17 11:51:23 -0400

    move filters into separate module

---
+++ birds/filters.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+# -*- mode: python -*-
+from django_filters import rest_framework as filters
+from django_filters.rest_framework import DjangoFilterBackend
+from django_filters.views import FilterView
+
+from birds.models import Animal, Event, Pairing, Sample
+
+
+class AnimalFilter(filters.FilterSet):
+    uuid = filters.CharFilter(field_name="uuid", lookup_expr="istartswith")
+    color = filters.CharFilter(field_name="band_color__name", lookup_expr="iexact")
+    band = filters.NumberFilter(field_name="band_number", lookup_expr="exact")
+    species = filters.CharFilter(field_name="species__code", lookup_expr="iexact")
+    plumage = filters.CharFilter(field_name="plumage__name", lookup_expr="icontains")
+    living = filters.BooleanFilter(field_name="alive", method="is_alive")
+    available = filters.BooleanFilter(field_name="reserved_by", lookup_expr="isnull")
+    reserved_by = filters.CharFilter(
+        field_name="reserved_by__username", lookup_expr="iexact"
+    )
+    parent = filters.CharFilter(field_name="parents__uuid", lookup_expr="istartswith")
+    child = filters.CharFilter(field_name="children__uuid", lookup_expr="istartswith")
+
+    def is_alive(self, queryset, name, value):
+        return queryset.filter(alive__gt=0)
+
+    class Meta:
+        model = Animal
+        fields = ["sex"]
+
+
+class EventFilter(filters.FilterSet):
+    animal = filters.CharFilter(field_name="animal__uuid", lookup_expr="istartswith")
+    color = filters.CharFilter(
+        field_name="animal__band_color__name", lookup_expr="iexact"
+    )
+    band = filters.NumberFilter(field_name="animal__band_number", lookup_expr="exact")
+    species = filters.CharFilter(
+        field_name="animal__species__code", lookup_expr="iexact"
+    )
+    status = filters.CharFilter(field_name="status__name", lookup_expr="istartswith")
+    location = filters.CharFilter(field_name="location__name", lookup_expr="icontains")
+    entered_by = filters.CharFilter(
+        field_name="entered_by__username", lookup_expr="icontains"
+    )
+    description = filters.CharFilter(field_name="description", lookup_expr="icontains")
+
+    class Meta:
+        model = Event
+        fields = {
+            "date": ["exact", "year", "range"],
+        }
+
+
+class PairingFilter(filters.FilterSet):
+    active = filters.BooleanFilter(field_name="active", method="is_active")
+    sire = filters.CharFilter(field_name="sire__uuid", lookup_expr="istartswith")
+    sire_color = filters.CharFilter(
+        field_name="sire__band_color__name", lookup_expr="iexact"
+    )
+    sire_band = filters.NumberFilter(
+        field_name="sire__band_number", lookup_expr="exact"
+    )
+    dam = filters.CharFilter(field_name="dam__uuid", lookup_expr="istartswith")
+    dam_color = filters.CharFilter(
+        field_name="dam__band_color__name", lookup_expr="iexact"
+    )
+    dam_band = filters.NumberFilter(field_name="dam__band_number", lookup_expr="exact")
+    description = filters.CharFilter(field_name="description", lookup_expr="icontains")
+
+    def is_active(self, queryset, name, value):
+        return queryset.filter(ended__isnull=value)
+
+
+class SampleFilter(filters.FilterSet):
+    uuid = filters.CharFilter(field_name="uuid", lookup_expr="istartswith")
+    type = filters.CharFilter(field_name="type__name", lookup_expr="istartswith")
+    location = filters.CharFilter(
+        field_name="location__name", lookup_expr="istartswith"
+    )
+    available = filters.BooleanFilter(field_name="location", method="is_available")
+    color = filters.CharFilter(
+        field_name="animal__band_color__name", lookup_expr="iexact"
+    )
+    band = filters.NumberFilter(field_name="animal__band_number", lookup_expr="exact")
+    species = filters.CharFilter(
+        field_name="animal__species__code", lookup_expr="iexact"
+    )
+    collected_by = filters.CharFilter(
+        field_name="collected_by__username", lookup_expr="iexact"
+    )
+
+    def is_available(self, queryset, name, value):
+        return queryset.exclude(location__isnull=True)
+
+    class Meta:
+        model = Sample
+        fields = {
+            "date": ["exact", "year", "range"],
+        }
--- birds/views.py
+++ birds/views.py
@@ -14,8 +14,6 @@ from django.urls import reverse
 from django.utils import dateparse
 from django.views import generic
 from django.views.decorators.http import require_http_methods
-from django_filters import rest_framework as filters
-from django_filters.views import FilterView
 from drf_link_header_pagination import LinkHeaderPagination
 from rest_framework import generics
 from rest_framework.decorators import api_view
@@ -42,77 +40,19 @@ from birds.serializers import (
     AnimalSerializer,
     EventSerializer,
 )
+from birds.filters import (
+    AnimalFilter,
+    EventFilter,
+    SampleFilter,
+    PairingFilter,
+    DjangoFilterBackend,
+)
 
 
 class LargeResultsSetPagination(LinkHeaderPagination):
     page_size = 1000
     page_size_query_param = "page_size"
     max_page_size = 10000
-
-
-class AnimalFilter(filters.FilterSet):
-    uuid = filters.CharFilter(field_name="uuid", lookup_expr="istartswith")
-    color = filters.CharFilter(field_name="band_color__name", lookup_expr="iexact")
-    band = filters.NumberFilter(field_name="band_number", lookup_expr="exact")
-    species = filters.CharFilter(field_name="species__code", lookup_expr="iexact")
-    plumage = filters.CharFilter(field_name="plumage__name", lookup_expr="icontains")
-    living = filters.BooleanFilter(field_name="alive", method="is_alive")
-    available = filters.BooleanFilter(field_name="reserved_by", lookup_expr="isnull")
-    reserved_by = filters.CharFilter(
-        field_name="reserved_by__username", lookup_expr="iexact"
-    )
-    parent = filters.CharFilter(field_name="parents__uuid", lookup_expr="istartswith")
-    child = filters.CharFilter(field_name="children__uuid", lookup_expr="istartswith")
-
-    def is_alive(self, queryset, name, value):
-        return queryset.filter(alive__gt=0)
-
-    class Meta:
-        model = Animal
-        fields = ["sex"]
-
-
-class EventFilter(filters.FilterSet):
-    animal = filters.CharFilter(field_name="animal__uuid", lookup_expr="istartswith")
-    color = filters.CharFilter(
-        field_name="animal__band_color__name", lookup_expr="iexact"
-    )
-    band = filters.NumberFilter(field_name="animal__band_number", lookup_expr="exact")
-    species = filters.CharFilter(
-        field_name="animal__species__code", lookup_expr="iexact"
-    )
-    status = filters.CharFilter(field_name="status__name", lookup_expr="istartswith")
-    location = filters.CharFilter(field_name="location__name", lookup_expr="icontains")
-    entered_by = filters.CharFilter(
-        field_name="entered_by__username", lookup_expr="icontains"
-    )
-    description = filters.CharFilter(field_name="description", lookup_expr="icontains")
-
-    class Meta:
-        model = Event
-        fields = {
-            "date": ["exact", "year", "range"],
-        }
-
-
-class PairingFilter(filters.FilterSet):
-    active = filters.BooleanFilter(field_name="active", method="is_active")
-    sire = filters.CharFilter(field_name="sire__uuid", lookup_expr="istartswith")
-    sire_color = filters.CharFilter(
-        field_name="sire__band_color__name", lookup_expr="iexact"
-    )
-    sire_band = filters.NumberFilter(
-        field_name="sire__band_number", lookup_expr="exact"
-    )
-    dam = filters.CharFilter(field_name="dam__uuid", lookup_expr="istartswith")
-    dam_color = filters.CharFilter(
-        field_name="dam__band_color__name", lookup_expr="iexact"
-    )
-    dam_band = filters.NumberFilter(field_name="dam__band_number", lookup_expr="exact")
-    description = filters.CharFilter(field_name="description", lookup_expr="icontains")
-
-    def is_active(self, queryset, name, value):
-        return queryset.filter(ended__isnull=value)
 
 
 @require_http_methods(["GET"])
@@ -831,34 +771,6 @@ def event_summary(request, year: int, month: int):
     )
 
 
-class SampleFilter(filters.FilterSet):
-    uuid = filters.CharFilter(field_name="uuid", lookup_expr="istartswith")
-    type = filters.CharFilter(field_name="type__name", lookup_expr="istartswith")
-    location = filters.CharFilter(
-        field_name="location__name", lookup_expr="istartswith"
-    )
-    available = filters.BooleanFilter(field_name="location", method="is_available")
-    color = filters.CharFilter(
-        field_name="animal__band_color__name", lookup_expr="iexact"
-    )
-    band = filters.NumberFilter(field_name="animal__band_number", lookup_expr="exact")
-    species = filters.CharFilter(
-        field_name="animal__species__code", lookup_expr="iexact"
-    )
-    collected_by = filters.CharFilter(
-        field_name="collected_by__username", lookup_expr="iexact"
-    )
-
-    def is_available(self, queryset, name, value):
-        return queryset.exclude(location__isnull=True)
-
-    class Meta:
-        model = Sample
-        fields = {
-            "date": ["exact", "year", "range"],
-        }
-
-
 @require_http_methods(["GET"])
 def sample_type_list(request):
     qs = SampleType.objects.all()
@@ -927,7 +839,7 @@ def api_info(request, format=None):
 class APIAnimalsList(generics.ListAPIView):
     queryset = Animal.objects.all()
     serializer_class = AnimalSerializer
-    filter_backends = (filters.DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend,)
     filterset_class = AnimalFilter
 
 
@@ -950,7 +862,7 @@ class APIAnimalDetail(generics.RetrieveAPIView):
 class APIEventsList(generics.ListAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-    filter_backends = (filters.DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend,)
     filterset_class = EventFilter
 
 
@@ -961,7 +873,7 @@ class APIAnimalPedigree(generics.ListAPIView):
     """
 
     serializer_class = AnimalPedigreeSerializer
-    filter_backends = (filters.DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend,)
     filterset_class = AnimalFilter
     pagination_class = LargeResultsSetPagination
 
