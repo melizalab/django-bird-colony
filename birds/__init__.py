7ea71f16ab0be9c8db551c64bf7641e0f58c1cc6
Dan Meliza <dan@meliza.org>
2024-12-18 20:07:19 -0500

    try moving version def to pyproject.toml

--- birds/__init__.py
+++ birds/__init__.py
@@ -1,4 +1,15 @@
 # -*- coding: utf-8 -*-
 # -*- mode: python -*-
-__version__ = "0.12.1"
+try:
+    from importlib.metadata import version
+    __version__ = version("django-bird-colony")
+except ImportError:
+    # For Python < 3.8
+    from importlib_metadata import version
+    __version__ = version("django-bird-colony")
+except Exception:
+    # If package is not installed (e.g. during development)
+    __version__ = "unknown"
 api_version = "1.0"
+
+
