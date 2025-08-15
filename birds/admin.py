6048eafe794f0be647c3dd9a4159bfb38cb54608
Dan Meliza <dan@meliza.org>
2025-08-15 15:45:54 -0400

    add autocomplete to some admin views

--- birds/admin.py
+++ birds/admin.py
@@ -9,6 +9,7 @@ class ParentInline(admin.TabularInline):
     fk_name = "child"
     max_num = 2
     min_num = 0
+    autocomplete_fields = ("parent",)
 
 
 class MeasurementInline(admin.TabularInline):
@@ -28,13 +29,13 @@ class AnimalAdmin(admin.ModelAdmin):
     )
     list_display = ("name", "species", "band", "uuid", "sex", "plumage", "reserved_by")
     list_filter = ("species", "sex", "band_color", "plumage", "reserved_by")
-    search_fields = ("band_number", "uuid", "plumage", "attributes__icontains")
     inlines = (ParentInline,)
 
 
 class EventAdmin(admin.ModelAdmin):
     date_hierarchy = "date"
     fields = ("animal", "status", "location", "description", "date", "entered_by")
+    autocomplete_fields = ("animal",)
     list_display = ("animal", "date", "status", "location", "description", "entered_by")
     list_filter = (
         "entered_by",
@@ -70,6 +71,7 @@ class SampleAdmin(admin.ModelAdmin):
 class PairingAdmin(admin.ModelAdmin):
     date_hierarchy = "began_on"
     fields = ("sire", "dam", "began_on", "purpose", "ended_on", "comment")
+    autocomplete_fields = ("sire", "dam")
     list_display = ("sire", "dam", "began_on", "purpose", "ended_on", "comment")
     list_filter = (
         "began_on",
